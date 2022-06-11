4e04a50833e6b6e25f19252fc307a8300a29cadc
lrl3036a <leonard.larochette@etu.iut-tlse3.fr>
2022-06-11 18:24:05 +0200

    ajout du fichier de reset du mdp par mail

--- php/connexion.php
+++ php/connexion.php
@@ -1,27 +1,28 @@
 <?php
  session_start();
-
+ require("connexionDB.php");
  $echec_connexion = false;
  ;
 
  if (isset($_SESSION['login']))
-    header("location:public/index.html");
+    header("location:../public/index.html");
 
     if(!empty($_POST['login']) && !empty($_POST['password'])){
        
-        require("connexionDB.php")
+        $linkpdo = connexion() ;
         
-        $req = $linkpdo ->prepare('select login, password from Utilisateur where login like :login ;');
+        $req = $linkpdo->prepare('select login, password from Utilisateur where login like :login ;');
 
-        if($req -> execute(array('login' => $_POST['login']))){
+        if($req->execute(array('login' => $_POST['login']))){
             $connexion = $req -> fetch();
             
 
-            if($connexion['password'] == md5($_POST['password'])){
+            if(md5($connexion['password']) == md5($_POST['password'])){
                 $_SESSION['login']=$_POST['login'];
-                header("location:public/index.html");
+                header("location:../public/index.html");
             }else{
                 $echec_connexion = true;
+                echo "connexion échoué";
             }
         }
     }
---
+++ php/reset.php
@@ -0,0 +1,67 @@
+<?php
+require(connexionBD.php);
+
+if (!empty($_POST['email'])){
+    $email_recup = htmlspecialchars($_POST['email']);
+    $linkpdo = connexion();
+
+    if(filter_var($email_recup,FILTER_VALIDATE_EMAIL)){
+        $req = $linkpdo->prepare('Select login from utilisateur where mail like :mail');
+        $req->execute(array(email_recup));
+        $cpt_mail_existant = $req ->rowCount();
+        if($cpt_mail_existant == 1){
+            $res = $req->fetch();
+            $login = $res['login'];
+
+            $_SESSION['email'] = $email_recup;
+            $recup_code = "";
+            for($i=0; $i < 8 ; $i++){
+                $recup_code .= mt_rand(0,9);
+            }
+            $recuper_insert = $linkpdo->prepare('Update utilisateur set tokenréinit = :token where mail = :mail');
+            $recup_insert->execute((array($recup_code, $email_recup)));
+
+            $header="MIME-Version: 1.0\r\n";
+            $header.='From: GPX_FINDER<agence.2bcl@gmail.com>'."\n";
+            $header.='Content-Type:test/html; charset="utf-8"'."\n";
+            $header.='Content-Transfer-Encoding: 8bit';
+            $message = '
+            <html>
+            <head>
+                <title>Récupération du mot de passe - GPX_FINDER</title>
+                <meta charset="utf-8" />
+            </head>
+            <body>
+                <font color="#303030";>
+                    <div align="center">
+                        <table width="600px">
+                            <tr>
+                                <td>
+                                    <div align="center">Bonjour <b>'.$login.'</b>,</div>
+                                    Voici votre code de récupération: <b>'.$recup_code.'</b>
+                                    A bientôt sur <a href="#">Gpx_Finder.com</a> !
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align="center">
+                                    <font size="2">
+                                        Ceci est un email automatique, merci de ne pas y répondre
+                                    </font>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </font>
+            </body>
+            </html>
+            ';
+            mail($email_recup, "Récupération de mot de passe - GPX_Finder", $message, $header);
+                //header("Location:")
+        }else{
+            $erreur = "Cette adresse mail ne correspond à aucun utilisateur";
+        }
+    }else{
+        $erreur = "Veuillez entrer votre adresse mail";
+    }
+}
