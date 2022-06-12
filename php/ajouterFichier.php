dd717c3f5640847cd6d85e840fdd764d118a7ca1
blp1775a <pierre.bayle@etu.iut-tlse3.fr>
2022-06-12 19:13:02 +0200

    Ajout d'un fichier à un repertoire existant

--- php/ajouterFichier.php
+++ php/ajouterFichier.php
@@ -3,6 +3,7 @@
 require 'config.php';
 require 'connexionDB.php';
 require('PointsGPX.php');
+
 session_start();
 
 if (!isset($_SESSION['variables_fichiers'])) {
@@ -22,15 +23,36 @@ $linkpdo = connexion();
 
 //ajouter une fichier dans la base de données
 if (isset($_POST['parcours'])) {
-
-    $parcours = $_POST['parcours'];
-    $sql = "INSERT INTO fichier VALUES (?,?,?,?,?,?,?,?,?,?)";
+    $id_parcours = $_POST['parcours'];
+    $sql = "INSERT INTO fichier(Id_Parcours,Description,Distance,Date_parcours,Ville_depart,Duree,Type_activite,Meteo,Denivele) VALUES (:id,:description,:distance,:date,:ville,:duree,:activite,:meteo,:denivele)";
     $stmt = $linkpdo->prepare($sql);
-    $stmt->execute(["", $parcours, $description, $distance, $date_parcours, $ville_depart, $type_activite, $meteo, $denivele_parcours, $duree]);
+    if(!$stmt->execute(
+        array(
+        'id' => $id_parcours,
+        'description' => $description,
+        'distance'=> round($distance,2),
+        'date' =>  $date_parcours,
+        'ville' => $ville_depart,
+        'duree' => $duree,
+        'activite' => $type_activite,
+        'meteo' => $meteo,
+        'denivele' =>  $denivele_parcours
+        )
+        )){
+            echo "<br> Debug requete fichier : <br><pre>";
+            echo $stmt->debugDumpParams();
+            echo "</pre>";
+        }
+
+
 }
 $liste_points = $_SESSION['tableauPoints'];
-$lastid = getLastId();
-echo $lastid;
+
+$query = "select LAST_INSERT_ID()";     
+$preparation_query = $linkpdo->prepare($query);
+$preparation_query->execute(); 
+$lastid = $preparation_query->fetch();
+$lastid = $lastid[0];
 
 if(!empty($liste_points)){
                 
@@ -38,7 +60,7 @@ if(!empty($liste_points)){
         $query = "insert into POINT_GPX(timecode,altitude,latitude,longitude,type,Id_Fichier) values (:timestamp,:alt,:lat,:long,:type,:id_fichier)";
         $preparation_query = $linkpdo->prepare($query);
 
-        if($preparation_query->execute(array(
+        if(!$preparation_query->execute(array(
             'timestamp' => $point->getTime(),
             'alt' => $point->getEle(),
             'lat' => $point->getLat(),
@@ -46,9 +68,7 @@ if(!empty($liste_points)){
             'type' => 'trkpt',
             'id_fichier' => $lastid
         )))
-        {
-            echo "Point enregistré <br><br>";
-        } else {	
+       {	
             echo "Erreur : <br>";
             echo "<pre>".$preparation_query->debugDumpParams()."</pre>";
             echo "<br><br>";
@@ -58,40 +78,41 @@ if(!empty($liste_points)){
     echo "Aucuns points à ajouter <br>";
 }
 
+header ('location:../public/html/show.html?parcours='.$id_parcours);
 
+// $sql = "SELECT * from fichier where Id_Fichier =".$lastid;
+// $stmt = $linkpdo->prepare($sql);
+// $stmt->execute();
 
+// //if no value returned, then the fichier doesn't exist
+// if ($stmt->rowCount() == 0) {
+//     echo "Erreur , Fichier non crée";
+// } else {
+//     header ('Location: ../public/html/show.html?parcours='.$id_parcours);
+// }
 
-$sql = "SELECT * from fichier where Id_Fichier =".$lastid;
-$stmt = $linkpdo->prepare($sql);
-$stmt->execute();
+// function idDuDernierFichier() {
+//     //recuperer l'id du fichier qui vient d'etre ajouté a la base de données 
+//     $sql = "SELECT Id_Fichier FROM fichier ORDER BY Id_Fichier DESC LIMIT 1";
+//     $linkpdo = connexion();
+//     $result = $linkpdo->prepare($sql);
+//     $result->execute();
+//     $id_fichier = $result->fetchColumn();
+//     return $id_fichier;
+// }
 
-//if no value returned, then the fichier doesn't exist
-if ($stmt->rowCount() == 0) {
-    echo "Erreur , Fichier non crée";
-} else {
-    //header ('Location: ../public/html/show.html?parcours='.$_POST['parcours']);
-}
-
-function idDuDernierFichier() {
-    //recuperer l'id du fichier qui vient d'etre ajouté a la base de données 
-    $sql = "SELECT Id_Fichier FROM fichier ORDER BY Id_Fichier DESC LIMIT 1";
-    $linkpdo = connexion();
-    $result = $linkpdo->prepare($sql);
-    $result->execute();
-    $id_fichier = $result->fetchColumn();
-    return $id_fichier;
-}
-
-function getLastId() {
-    $linkpdo = connexion();
-    $query = "select LAST_INSERT_ID()";     // permet de récupérer la clé primaire de la derniere ligne insérée une connexion
-    $preparation_query = $linkpdo->prepare($query);
-    $preparation_query->execute(); 
-    $cle_primaire = $preparation_query->fetch();
-    var_dump($cle_primaire);
-    $cle_primaire = $cle_primaire[0];
+// function getLastId() {
+//     $linkpdo = connexion();
+//     $query = "select LAST_INSERT_ID()";     // permet de récupérer la clé primaire de la derniere ligne insérée une connexion
+//     $preparation_query = $linkpdo->prepare($query);
+//     $preparation_query->execute(); 
+  
+     
+  
+//     var_dump($preparation_query->fetch());
+//     //$cle_primaire = $cle_primaire[0];
     
-    return $cle_primaire;
-}
+//     //return $cle_primaire;
+// }
 
 ?>
