e4c4288f8eddf13c79c43bd3eaab26906a20801b
lrl3036a <leonard.larochette@etu.iut-tlse3.fr>
2022-06-13 12:41:24 +0200

    avancement

--- php/reset.php
+++ php/reset.php
@@ -82,18 +82,22 @@ if (!empty($_POST['email'])){
             //envoi
             $mail->send();
             }catch(Exception $e){
-                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                header("Location:../public/html/reset.html?error=$erreur");
+                //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
             }
             //envoi vers la page de changement de mot de passe
             header("Location:../public/html/token.html?email=$email_recup");
+            //echo"essai";
         }else{
             $erreur = "Cette adresse mail ne correspond à aucun utilisateur";
+            header("Location:../public/html/reset.html?error=$erreur");
         }
     }else{
         $erreur = "Veuillez entrer une adresse mail valide";
+        header("Location:../public/html/reset.html?error=$erreur");
     }
 }else{
     $erreur = "Veuillez entrer une adresse mail valide";
+    header("Location:../public/html/reset.html?error=$erreur");
 }
-header("Location:../public/html/reset.html?error=$erreur");
 ?>
--- php/token.php
+++ php/token.php
@@ -2,8 +2,8 @@
  session_start();
  require("connexionDB.php");
 
- if (isset($_SESSION['login']))
- header("location:../public/index.html");
+ //if (isset($_SESSION['login']))
+// header("location:../public/index.html");
 
     if((!empty($_POST['token']) && (!empty($_POST['email'])))){
 
@@ -15,11 +15,12 @@
         $req -> execute(array('mail' =>$mail));
 
         $reset = $req ->fetch();
-        if($_POST['token'] == $reset){
-            header("Location:../public/html/changementMDP.html?token=$reset");
+        if($_POST['token'] == $reset[0]){
+            
+            header('Location:../public/html/changementMDP.html?token='.$reset[0]);
         }else{
-            header('Location:../public/html/token.html?error=token');
+            header('Location:../public/html/token.html?error=token&email='.$_POST["email"]);
         }
     }else{
-        header('Location:../public/html/token.html?error=token');
+        header('Location:../public/html/token.html?error=token&email='.$_POST["email"]);
     }
