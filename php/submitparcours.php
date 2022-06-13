af8af7df1d866678ac4c1fcd9ce1fa8eef4444ea
Samuel BERTIN <75686202+Samuel-Bertin@users.noreply.github.com>
2022-06-13 18:50:54 +0200

    gestion des erreurs formulaire ajouter

--- php/submitparcours.php
+++ php/submitparcours.php
@@ -3,14 +3,22 @@
     
 session_start();
 
+$_SESSION['erreurs'] = array();
 
- verifVariables();
+verifVariables();
+var_dump($_SESSION['erreurs']);
 
 if (isset($_POST['creer'])) {
     header('Location: ajout_points_nouveau_parcours.php');
 } else {
     if (isset($_POST['ajouter'])) {
-        header ('Location: ../public/html/addTo.html');
+        //si la taille du tableau $_SESSION['erreurs'] est vide, on peut ajouter le parcours
+        if (empty($_SESSION['erreurs'])) {
+            header('Location: ../public/html/addTo.html');
+        } else {
+            header ('Location: ../public/html/add.html');
+        }
+        
     }
 }
 
@@ -18,8 +26,15 @@ function verifVariables() {
 
     if (isset($_POST['nom'])) {
         $nom = $_POST['nom'];
+        //Le nom ne doit contenir que des lettres, des chiffres ou des espaces et doit faire entre 2 et 50 caractères
+        if (!preg_match("/^[a-zA-Z ]{2,50}$/", $nom)) {
+            $_SESSION['erreurs']['nom'] = "Le nom doit contenir entre 2 et 50 caractères non numériques";
+        } else {
+            //unset 
+            unset($_SESSION['erreurs']['nom']);
+        }
     } else {
-        $nom = "";
+        $_SESSION['erreurs']['nom'] = "Veuillez entrer un nom";
     }
     $_SESSION['nom'] = $nom;
 
@@ -39,43 +54,85 @@ function verifVariables() {
 
     if (isset($_POST['distance'])) {
         $distance = $_POST['distance'];
+        //enlever "km" de la string $distance
+        $distance = str_replace(" km", "", $distance);
+        // la distance doit être un nombre
+        if (!is_numeric($distance)) {
+            unset($_SESSION['erreurs']['distance']);
+            $_SESSION['erreurs']['distance'] = "La distance doit être un nombre";
+        } else {
+            //unset 
+            unset($_SESSION['erreurs']['distance']);
+        }
+        
     } else {
-        $distance = "";
+        $_SESSION['erreurs']['distance'] = "La distance est obligatoire";
     }
     $_SESSION['distance'] = $distance;
 
     if (isset($_POST['duree'])) {
         $duree = $_POST['duree'];
+        //La durée ne peut contenir que des valeurs numériques et faire au moins un caractere
+        if (!preg_match('/^[0-9:]*$/', $duree)) {
+            unset($_SESSION['erreurs']['duree']);
+            $_SESSION['erreurs']['duree'] = "La durée doit contenir que des chiffres";
+        } else if (strlen($duree) == 0) {
+            $_SESSION['erreurs']['duree'] = "La durée doit contenir au moins un caractere";
+        } else {
+            //unset 
+            unset($_SESSION['erreurs']['duree']);
+        }
     } else {
-        $duree = "";
+        $duree = "0";
     }
     $_SESSION['duree'] = $duree;
 
     if (isset($_POST['denivele'])) {
         $denivele_parcours = $_POST['denivele'];
+        //Le denivele ne peut contenir que des valeurs numérique, des points et faire au moins un caractere
+        if (!preg_match('/^[0-9.]*$/', $denivele_parcours)) {
+            unset($_SESSION['erreurs']['denivele']);
+            $_SESSION['erreurs']['denivele'] = "Le denivele doit contenir que des chiffres";
+        } else if (strlen($denivele_parcours) == 0) {
+            $_SESSION['erreurs']['denivele'] = "Le denivele doit contenir au moins un caractere";
+        } else {
+            //unset 
+            unset($_SESSION['erreurs']['denivele']);
+        }
     } else {
-        $denivele_parcours = "";
+        //erreur si le denivele n'est pas renseigné
+        $_SESSION['erreurs']['denivele'] = "Le denivele est obligatoire";
     }
     $_SESSION['denivele'] = $denivele_parcours;
 
-    if (isset($_POST['date'])) {
+    if (isset($_POST['date']) && !empty($_POST['date'])) {
         $date_parcours = $_POST['date'];
+        unset($_SESSION['erreurs']['date']);
     } else {
-        $date_parcours = "";
+        //La date ne doit pas etre vide
+        $_SESSION['erreurs']['date'] = "La date est obligatoire";
     }
     $_SESSION['date'] = $date_parcours;
 
-    if (isset($_POST['ville'])) {
-        $ville = $_POST['ville'];
+    if (isset($_POST['ville']) && !empty($_POST['ville'])) {
+        $date_parcours = $_POST['ville'];
+        unset($_SESSION['erreurs']['ville']);
     } else {
-        $ville = "";
+        //La ville ne doit pas etre vide
+        $_SESSION['erreurs']['ville'] = "La ville est obligatoire";
     }
-    $_SESSION['ville'] = $ville;
+    $_SESSION['ville'] = $date_parcours;
 
-    if (isset($_POST['activite'])) {
+    if (isset($_POST['activite']) && !empty($_POST['activite'])) {
         $activite = $_POST['activite'];
+        //l'activite ne doit pas contenir des nombres ni etre vide  
+        if (!preg_match('/^[a-zA-Z áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ]*$/', $activite)) {
+            unset($_SESSION['erreurs']['activite']);
+            $_SESSION['erreurs']['activite'] = "L'activite doit contenir que des lettres";
+        }
     } else {
-        $activite = "";
+        //le nom d'activité de doit pas etre vide
+        $_SESSION['erreurs']['activite'] = "L'activite est obligatoire";
     }
     $_SESSION['activite'] = $activite;
 
