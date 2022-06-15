a458c88134658bd43631c36b6ef3f821dbf0ab1b
blp1775a <pierre.bayle@etu.iut-tlse3.fr>
2022-06-15 23:44:47 +0200

    correction edit

---
+++ php/utils/verifIntegrite.php
@@ -0,0 +1,17 @@
+    <?php 
+
+    if(!isset($_SESSION['connecte'])){
+        header('location:login.html');
+    }
+    
+    function verifier_repertoir_non_vide($id_parcours){
+        $linkpdo = connexion();
+        $req = (" SELECT count(id_fichier) as nb_fichiers from fichier where id_parcours =".$id_parcours);
+        $res = $linkpdo->prepare($req);
+        $res->execute();
+        $data = $res->fetch();
+
+        return $data['nb_fichiers'];
+    }
+    
+    ?>
