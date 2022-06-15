7e2faa96d5f049a09fb3fab6be32ba5b78ce5140
Samuel BERTIN <75686202+Samuel-Bertin@users.noreply.github.com>
2022-06-15 15:39:48 +0200

    fix de la recherche

--- php/rechercheLibre.php
+++ php/rechercheLibre.php
@@ -1,5 +1,7 @@
 <?php
 
+    require 'connexionDB.php';
+
     function creerRequeteRechercheLibre() {
 
         if (!isset($_POST['recherche'])) {
@@ -24,7 +26,78 @@
         return $sql;
     }
 
-    echo creerRequeteRechercheLibre();
+    function recupDonneesAffichageRechercheLibre(){
+
+        $linkpdo=connexion();
+        $sql = creerRequeteRechercheLibre();
+        $req=$linkpdo->prepare($sql.' order by date_parcours DESC;');
+    
+        $req->execute();
+        $nb_lignes = $req->rowCount();
+        
+        $retour=array();
+        while($data = $req->fetch()){
+            
+    
+            switch($data[6]){
+                case 0: $groupe = "non";
+                break;
+                case 1: $groupe = "oui";
+            }
+            
+            switch($data[7]){
+                case 0: $ht = "non";
+                break;
+                case 1: $ht = "oui";
+            }
+            $valeurs = array('date' => date_beau($data[0]),
+                            'nom' => $data[1],
+                            'activite' => $data[2],
+                            'temps' =>temps_beau($data[3]),
+                            'denivele' =>denivele_beau($data[4]),
+                            'depart' => $data[5],
+                            'auteur' => ucwords('vous'),
+                            'groupe' => $groupe,
+                            'trainer' => $ht,
+                            'meteo' => $data[8],
+                            'id_fichier' => $data[9],
+                            'id_parcours' => $data[10]);
+            
+        $nb=array_push($retour,$valeurs);
+        
+        }
+        return $retour;
+        
+    }
+    
+    function temps_beau($temps){
+        $arr=str_split($temps, 3);
+        $retour="";
+        $retour.=str_replace(':','h',$arr[0]);
+        $retour.=str_replace(':','m',$arr[1]);
+        $retour.=$arr[2];
+        $retour.='s';
+        return $retour;
+    }
+    
+    function denivele_beau($denivele){
+        $retour= str_replace('.',',', $denivele);
+        $retour.=' m';
+        return $retour;
+    }
+    
+    function date_beau($date){
+        $arr=explode(' ',$date);
+        $date2 = $arr[0];
+        $arr2=explode("-", $date2);
+        $retour="";
+        $retour.=$arr2[2];
+        $retour.="/";
+        $retour.=$arr2[1];
+        $retour.="/";
+        $retour.=$arr2[0];
+        return $retour;
+    }
 
     
 ?>
